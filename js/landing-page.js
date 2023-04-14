6963df7453a2e900dcaeec1600a3e8c2c8161995
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-04-14 20:56:32 +0200

    Update js/css modules
    - Upgrade to bootstrap 5.2.3
    - Upgrade to jquery 3.6.4
    - Upgrade to font-awesome 4.7.0
    - Delete unused files
    - Added npm and simple script to update the dependencies
    
    Fixes https://github.com/pmd/pmd.github.io/security/dependabot/1

--- js/landing-page.js
+++ js/landing-page.js
@@ -9,24 +9,3 @@ $(function() {
         event.preventDefault();
     });
 });
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
